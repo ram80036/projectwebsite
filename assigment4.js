08a98b39f6fff37312d9b25e3b9d008d993795f7
ram80036 <71321718+ram80036@users.noreply.github.com>
2020-09-22 14:09:59 +0530

    Add files via upload

---
+++ assigment4.js
@@ -0,0 +1,11 @@
+var name=["ram","remo","jenny","ben","tom"];
+for (var i = 0; i < name.length; i++){
+    fname=name[i]
+    if(fname.charAt(0)=="j" || fname.charAt(0)=="J"){
+        console.log("goodbye " +fname)
+    }else{
+        console.log("Hello "+fname)
+    }
+       
+    
+}
